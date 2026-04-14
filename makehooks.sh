db8f5bc21f5d650736892e0cabbfae7cd58dd47a
Jon R. Humphrey <jon@humphrey.me.uk>
2026-04-14 21:44:32 +0100

    refactor(githooks): relocate hook scripts and update installation

--- makehooks.sh
+++ makehooks.sh
@@ -9,25 +9,25 @@ echo "Cloning the Epimorphics githooks from the repo into ${tmp_dir} ..."
 git clone -q git@github.com:epimorphics/githooks.git "${tmp_dir}"
 
 echo "Creating new hooks directory ..."
-mkdir -p ./.github/hooks/
+mkdir -p ./.githooks/
 
 echo "Copying hooks from cloned repository ..."
-cp -R -fi "${tmp_dir}"/hooks/ ./.github/hooks/
+cp -R -fi "${tmp_dir}"/hooks/ ./.githooks/
 
 echo "Removing temporary directory ..."
 rm -rf "${tmp_dir}"
 
 echo "Renaming hooks and setting permissions..."
-for f in ./.github/hooks/*; do mv -f "$f" "${f%.sh}"; done
-for f in ./.github/hooks/*; do chmod +x "$f"; done
+for f in ./.githooks/*; do mv -f "$f" "${f%.sh}"; done
+for f in ./.githooks/*; do chmod +x "$f"; done
 
 echo "Setting hooks path to git config ..."
-git config core.hooksPath './.github/hooks'
+git config core.hooksPath './.githooks'
 
 echo "Adding new hooks to source control ..."
 git add .
 
 echo "Committing hooks ..."
-git commit -m "Added git hooks to ${PWD}"
+git commit -m "Added git hooks to ${NAME}"
 
 echo "Git hooks have been added to ${NAME} and will now run the pre-commit hook"
